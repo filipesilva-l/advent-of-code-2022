83b1c0c6699c894139108f3b20096c4e0e7f8c9b
Filipe Silva <fil.lourenco.silva@gmail.com>
2022-12-01 15:36:46 -0300

    day 1 resolution

--- day1/Program.cs
+++ day1/Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿var lines = File.ReadAllLines("input");
+
+List<long> elves = new(lines.Count());
+
+long currentTotal = 0;
+foreach (var line in lines)
+{
+	if (string.IsNullOrEmpty(line))
+	{
+		elves.Add(currentTotal);
+		currentTotal = 0;
+		continue;
+	}
+
+	Console.WriteLine("Input: {0}", line);
+
+	var totalInLine = long.Parse(line);
+	
+	currentTotal += totalInLine;
+}
+
+Console.WriteLine("Total elfs: {0}", elves.Count());
+
+var sorted = elves.OrderByDescending(t => t);
+
+var totalMostCalories = sorted.Take(3).Sum();
+
+Console.WriteLine("Total most calories: {0}", totalMostCalories);
+
