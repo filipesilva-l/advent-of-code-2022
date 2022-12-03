7e8ed31be284173ca8094e4b6e9a3d26039086b7
Filipe Silva <fil.lourenco.silva@gmail.com>
2022-12-03 14:31:09 -0300

    day3 part 1 resolution

---
+++ day3/Program.cs
@@ -0,0 +1,22 @@
+﻿var rucksacks = File.ReadAllLines("input");
+
+var totalPriority = 0;
+foreach (var rucksack in rucksacks)
+{
+	var firstCompartment = rucksack.Substring(0, rucksack.Length / 2);
+	var lastCompartment = rucksack.Substring(rucksack.Length / 2);
+
+	var letters = firstCompartment.Intersect(lastCompartment);
+
+	totalPriority += letters.Select(l => GetPriority(l.ToString())).Sum();
+}
+
+Console.WriteLine("total: {0}", totalPriority);
+
+
+int GetPriority(string letter)
+{
+	var alphabet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+	return alphabet.IndexOf(letter) + 1;
+}
